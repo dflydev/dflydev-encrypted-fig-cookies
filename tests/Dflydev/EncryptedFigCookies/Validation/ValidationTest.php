2a5655be42a863645c46856048b30ab6f938ec61
Beau Simensen <beau@dflydev.com>
2015-06-04 11:13:41 -0500

    Add more information to test.

--- tests/Dflydev/EncryptedFigCookies/Validation/ValidationTest.php
+++ tests/Dflydev/EncryptedFigCookies/Validation/ValidationTest.php
@@ -11,11 +11,16 @@ class ValidationTest extends \PHPUnit_Framework_TestCase
         $key = 'E26m218TLqgJeY40ydCET10tMUD6qSlV';
         $nonce = 'Z6vsz6UqTtqYcPy4TRinVtb8ShsVvDvq';
         $value = 'hello world!';
+
+        // We know how the hmac should be created.
         $hmac = hash_hmac($algo, $key, $nonce.$value);
+
+        // We know what the signed value should be.
+        $signedValue = $nonce.$value.'.'.$hmac;
 
         $validation = new Validation($key, $algo);
 
-        $extractedValue = $validation->extract($nonce.$value.'.'.$hmac);
+        $extractedValue = $validation->extract($signedValue);
 
         $this->assertEquals($value, $extractedValue);
     }
